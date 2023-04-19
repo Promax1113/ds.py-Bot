1042cf9fc3902ac71925b46842bcfdadeb91d191
Adrià Tenorio <aaten@MacBook-Pro-de-Sergio.local>
2023-04-19 23:06:25 +0200

    Started coding bot.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import discord
+
+ds_token = "7DSbOv81IVAVHMA2_R53zh-hAgj07XlG"
+
+
+class  myClient(discord.Client):
+    async def on_ready(self):
+        print("Logged in successfully!")
+
+intents = discord.Intents.default()
+client = myClient(intents=intents)
+client.run(token=ds_token)
